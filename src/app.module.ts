e6c45d4f8c634c4f0d31c4f8511110a4b58a92d8
Frank Strieter <fs@co-it.eu>
2018-04-19 13:06:39 +0200

    removes unused AppController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { ArticleModule } from 'articles/article.module';
 
 @Module({
   imports: [ArticleModule],
-  controllers: [AppController],
+  controllers: [],
 })
 export class AppModule {}
